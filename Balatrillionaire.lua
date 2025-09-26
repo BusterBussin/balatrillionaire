69ed0362d3a7f924512206fbc992bf04cd63abd5
Buster <busterbussin@gmail.com>
2025-09-26 12:13:46 -0400

    final changes

--- Balatrillionaire.lua
+++ Balatrillionaire.lua
@@ -8,8 +8,8 @@ SMODS.Atlas{
 SMODS.Joker{
     key        = "trillion_jokerofgreed",
     atlas      = "trillion_balatrillionaire",
-    rarity     = 4,
-    cost       = 15,
+    rarity     = 3,
+    cost       = 20,
     pos = {x = 0, y = 0},
     discovered = true,
     spawnable  = true,
@@ -52,7 +52,7 @@ SMODS.Joker{
         name = "Equal Exchange",
         text = {
             "For each Ace played, quadruples your {C:mult}multiplier{},",
-            "but costs {C:money}$7{} for each Ace."
+            "but costs {C:money}$10{} for each Ace."
         }
     },
 
@@ -68,12 +68,11 @@ SMODS.Joker{
 
             if ace_count > 0 then
                 -- subtract money directly (allows negative)
-                G.GAME.dollars = (G.GAME.dollars or 0) - (7 * ace_count)
+                G.GAME.dollars = (G.GAME.dollars or 0) - (10 * ace_count)
 
                 -- quadruple mult per Ace
                 return {
                     x_mult_mod = 4 ^ ace_count,
-                    message = "-$7",
                     message    = "Equal Exchange!"
                 }
             end
