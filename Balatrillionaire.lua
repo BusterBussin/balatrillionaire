ca311415af8024668fa3de9243589f2533059902
Buster <busterbussin@gmail.com>
2025-09-29 06:57:01 -0400

    Swashbuckler Riff Raff, Fix and balance Equal Exchange

--- Balatrillionaire.lua
+++ Balatrillionaire.lua
@@ -38,42 +38,53 @@ SMODS.Joker{
 }
 
 
-SMODS.Joker{
-    key        = "trillion_equalexchange",
-    atlas      = "trillion_balatrillionaire",
-    pos        = { x = 1, y = 0 },
-    rarity     = 4,
-    cost       = 20,
-    discovered = true,
-    spawnable  = true,
-    blueprint_compat = true,
-
-    loc_txt = {
-        name = "Equal Exchange",
-        text = {
-            "For each Ace played, quadruples your {C:mult}multiplier{},",
-            "but costs {C:money}$10{} for each Ace."
+SMODS.Joker{ --Equal Exchange
+    key = "trillion_equalexchange",
+    config = {
+        extra = {
+            dollars = 25,
+            Xmult = 4
         }
     },
+    loc_txt = {
+        ['name'] = 'Equal Exchange',
+        ['text'] = {
+            [1] = 'For each{C:green} ace{} played, quadruples your{C:red} mult{}.',
+            [2] = 'Lose {C:attention}$25{} per {C:green}ace{} played,'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 0,
+        y = 0
+    },
+    cost = 20,
+    rarity = 4,
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'trillion_balatrillionaire',
+    in_pool = function(self, args)
+          return (
+          not args 
+          or args.source ~= 'sho' and args.source ~= 'buf' and args.source ~= 'jud' and args.source ~= 'rif' 
+          or args.source == 'rta' or args.source == 'sou' or args.source == 'uta' or args.source == 'wra'
+          )
+          and true
+      end,
 
     calculate = function(self, card, context)
-        if context.joker_main then
-            local ace_count = 0
-            for _, c in ipairs(context.scoring_hand or {}) do
-                -- Ace has ID 14
-                if c.get_id and c:get_id() == 14 then
-                    ace_count = ace_count + 1
-                end
-            end
-
-            if ace_count > 0 then
-                -- subtract money directly (allows negative)
-                G.GAME.dollars = (G.GAME.dollars or 0) - (10 * ace_count)
-
-                -- quadruple mult per Ace
+        if context.individual and context.cardarea == G.play  then
+            if context.other_card:get_id() == 14 then
                 return {
-                    x_mult_mod = 4 ^ ace_count,
-                    message    = "Equal Exchange!"
+                    dollars = -card.ability.extra.dollars,
+                    extra = {
+                        Xmult = card.ability.extra.Xmult
+                        }
                 }
             end
         end
@@ -114,6 +125,81 @@ SMODS.Joker{ --Infinite Wealth
                 return {
                     dollars = card.ability.extra.dollars
                 }
+        end
+    end
+}
+SMODS.Joker{ --Riff Raff but better
+    key = "trillion_riffraffbutbetter",
+    config = {
+        extra = {
+            eternal = 0,
+            respect = 0,
+            yes = 0,
+            var1 = 0
+        }
+    },
+    loc_txt = {
+        name = 'Riff Raff but better',
+        text = {
+            [1] = 'Dumbass like {C:red}you{} ain\'t worth a kidney stone.',
+            [2] = 'Whatever, go my {C:dark_edition}Swashbucklers{}.'
+        },
+        unlock = { [1] = 'Unlocked by default.' }
+    },
+    pos = { x = 3, y = 0 },
+    cost = 4,
+    rarity = 1,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'trillion_balatrillionaire',
+
+    calculate = function(self, card, context)
+        -- only do something when setting the blind
+        if context.setting_blind then
+            return {
+                func = function()
+                    -- spawn Swashbuckler with negative edition and eternal sticker
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            local joker_card = SMODS.add_card({ set = 'Joker', key = 'j_swashbuckler' })
+                            if joker_card then
+                                joker_card:set_edition("e_negative", true)
+                                joker_card:add_sticker('eternal', true)
+                            end
+                            return true
+                        end
+                    }))
+
+                    -- find and remove Riff Raff if present
+                    for _, joker in ipairs(G.jokers.cards) do
+                        if joker.config.center.key == "j_riff_raff" and not joker.getting_sliced then
+                            if joker.ability.eternal then
+                                joker.ability.eternal = nil
+                            end
+                            joker.getting_sliced = true
+                            G.E_MANAGER:add_event(Event({
+                                func = function()
+                                    joker:start_dissolve({ G.C.RED }, nil, 1.6)
+                                    return true
+                                end
+                            }))
+                            card_eval_status_text(context.blueprint_card or card,
+                                'extra', nil, nil, nil,
+                                { message = "FUCK YOU", colour = G.C.RED })
+                            break
+                        end
+                    end
+
+                    card_eval_status_text(context.blueprint_card or card,
+                        'extra', nil, nil, nil,
+                        { message = "GO MY SWASHBUCKLER", colour = G.C.BLUE })
+
+                    return true
+                end
+            }
         end
     end
 }
